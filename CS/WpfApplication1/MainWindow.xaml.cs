7820ad019cab8d4bdf8d3d6321928a34e5f48df7
dxBot <DevExpressExampleBot>
2018-04-08 15:54:27 +0300

    initial commit

---
+++ CS/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using DevExpress.Xpf.Ribbon;
+
+namespace ThemeHelperWPF {
+    public partial class MainWindow : DXRibbonWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports DevExpress.Xpf.Ribbon
+
+Namespace ThemeHelperWPF
+    Partial Public Class MainWindow
+        Inherits DXRibbonWindow
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
